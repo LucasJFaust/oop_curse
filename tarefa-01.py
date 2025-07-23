656ac6bbfe440941b8e74533caaa0139b3228b0c
LucasJFaust <lucasjfm.eng@gmail.com>
2025-07-23 18:03:59 -0300

    Subindo uma atualização de  tarefa-01.py

---
+++ tarefa-01.py
@@ -0,0 +1,26 @@
+"""
+Identify 3 classes that might be needed for a program developed for a:
+
+Bank
+Donut Shop
+Videogame Store
+
+Think about the objects that a program might need in these scenarios.
+
+For example, a pizza store could represent a Pizza with a class and an Employee with another class.
+
+Format:
+
+Please write your answers in this format:  <context> : <class>, <class>, <class>
+
+For example: Pizza Store: Pizza, Employee, Topping
+
+Mark assignment as complete:
+"Once you have submitted your assignment, and you have navigated through each section to the 'Give Feedback' page, the assignment will be marked as complete with a gray checkmark."— source
+Write three classes for each scenario.
+"""
+# Small Bank: SavingsAccount, CheckingAccount, CreditCard.
+
+# Donut Shop: Donut, Soda, Topping.
+
+# Videogame Store: Videogame, Client, Employee.
