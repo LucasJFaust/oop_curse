d815d02ee755f5f3f057158a17745920bd51797a
LucasJFaust <lucasjfm.eng@gmail.com>
2025-07-23 17:54:32 -0300

    Subindo o main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Um classe é compasta de um Header e um Body.
+Class Header: é a primeira linha do arquivo, que contém o nome da classe, o número de alunos e o nome do professor.
+"""
+# Durente o curso vamos estar descrevedo a classe Backpack. Para escrever uma classe primeiro usams a palavra class, depois o nome da classe, e por último dois pontos.
+# class Backpack:
+#     pass # A palavra pass é usada para indicar que não há código dentro da classe por enquanto. E pode ser usada para criar uma classe vazia.
+
+# O body da classe é onde você define os atributos e métodos da classe. Atributos são as características da classe, e métodos são as ações que a classe pode realizar.
+#Dentro de uma classe podemos encontrar alguns elementos como Class Atributes, __init__() e Methods.
+class ClassName:
+
+    # Class Atributes são atributos que pertencem à classe como um todo, e não a uma instância específica da classe. Eles são definidos diretamente dentro da classe, mas fora de qualquer método.
+
+    # __init__() é um método especial que é chamado quando você cria uma instância da classe. Ele é usado para inicializar os atributos da classe.
+
+    # Methods são funções definidas dentro da classe que podem ser usadas para realizar ações ou manipular os dados da classe. Eles são definidos com a palavra-chave def e devem ter o primeiro parâmetro como self, que se refere à instância atual da classe.
