6fb0a8298a1b810ba59d6f24524ff8de60cb6e5b
LucasJFaust <lucasjfm.eng@gmail.com>
2025-07-29 21:00:46 -0300

    Atualizando o exemplo 6

--- 6-update_instance_attributes.py
+++ 6-update_instance_attributes.py
@@ -7,13 +7,29 @@ class Backpack:
         self.items = []
         self.color = color
 
-my_backpack = Backpack("Blue")  # Criando uma instância da classe Backpack com a cor "Blue"
-your_backpack = Backpack("Green")  # Criando outra instância com a cor "Green"
+# my_backpack = Backpack("Blue")  # Criando uma instância da classe Backpack com a cor "Blue"
+# your_backpack = Backpack("Green")  # Criando outra instância com a cor "Green"
 
-print(my_backpack.color)  # Imprime a cor inicial da mochila
-print(your_backpack.color)  # Imprime a cor da outra mochila
+# print(my_backpack.color)  # Imprime a cor inicial da mochila
+# print(your_backpack.color)  # Imprime a cor da outra mochila
 
-print("Atualizando atributos da instância... Só da instância my_backpack")
-my_backpack.color = "Red"  # Atualizando o atributo color da instância my_backpack
-print(my_backpack.color)
-print(your_backpack.color)
+# print("Atualizando atributos da instância... Só da instância my_backpack")
+# my_backpack.color = "Red"  # Atualizando o atributo color da instância my_backpack
+# print(my_backpack.color)
+# print(your_backpack.color)
+
+class Cirulo:
+    def __init__(self, raio, cor):
+        self.raio = raio
+        self.cor = cor
+
+meu_circulo = Cirulo(10, "Vermelho")  # Criando uma instância da classe Cirulo com raio 10 e cor "Vermelho"
+print(meu_circulo.raio)
+print(meu_circulo.cor)
+
+meu_circulo.raio = 15  # Atualizando o atributo raio da instância meu_circulo
+meu_circulo.cor = "Azul"  # Atualizando o atributo cor da instância meu_circulo
+
+print(meu_circulo.raio)  # Imprime 15, o novo valor de raio
+print(meu_circulo.cor)   # Imprime "Azul", o novo valor de cor
+
